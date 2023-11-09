afae84126168be2d353240d00cd4001498cb27cb
HIIKYY19 <nurrohmanrizky23@gmail.com>
2023-11-09 02:59:04 -1200

    mvc

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,10 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LihatNilaiController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\PagenotController;
+use App\Http\Controllers\KartuController;
+use App\Http\Controllers\JenisProdukController;
+use App\Http\Controllers\ProdukController;
+use App\Http\Controllers\PelangganController;
 
 /*
 |--------------------------------------------------------------------------
@@ -46,3 +50,12 @@ Route::get('/dataMahasiswa', [LihatNilaiController::class, 'dataMahasiswa']);
 Route::get('/dashboard', [DashboardController::class, 'index']);
 //contoh pemanggilan secara satu persatu function menggunakan get,put, update, delete
 Route::get('/notfound', [PagenotController::class, 'index']);
+
+//memanggil seluruh fungsi atau function menggunakan resource
+Route::resource('kartu', KartuController::class);
+//memanggil fungsi satu persatu
+Route::get('/jenis_produk', [JenisProdukController::class, 'index']);
+
+Route::get('/produk', [ProdukController::class, 'index']);
+
+Route::resource('pelanggan', PelangganController::class);
