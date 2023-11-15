858b8c1ca8f5972ef9a0a4a0d7179bd48bd57041
HIIKYY19 <nurrohmanrizky23@gmail.com>
2023-11-14 12:21:30 -1200

    Laravel Dashboard

--- app/Http/Controllers/ProdukController.php
+++ app/Http/Controllers/ProdukController.php
@@ -38,6 +38,42 @@ class ProdukController extends Controller
      */
     public function store(Request $request)
     {
+
+        $request->validate([
+            'kode' => 'required|unique:produk|max:10',
+            'nama' => 'required|max:45',
+            'harga_beli' => 'required|numeric',
+            'harga_jual' => 'required|numeric',
+            'stok' => 'required|numeric',
+            'min_stok' => 'required|numeric',
+            'foto' => 'nullable|image|mimes:jpg,jpeg,gif,png,svg|max:2048',
+            'deksripsi' => 'nullable|string|min:10',
+            'jenis_produk_id' => 'required|integer',
+        ],
+        [
+           
+            'kode.max' => 'Kode maximal 10 karakter',
+            'kode.required' => 'Kode Wajib Diisi',
+            'kode.unique' => 'Kode Sudah terisi pada data lain',
+            'nama.required' => 'Nama wajib diisi',
+            'nama.max' => 'Nama maksimal 45 karakter',
+            'harga_beli.required' => 'Harga beli Harus diisi',
+            'harga_beli.numeric' => 'Harus Angka',
+            'harga_jual.required' => 'Harga jual harus disii',
+            'harga_jual.numeric' => 'harus angka',
+            'stok.required' => 'Stok harus diisi',
+            'min_stok.required' => 'Minimal stok harus terisi',
+            'foto.max' => 'Maksimal 2 MB',
+            'foto.image' => 'File ekstensi harus jpg,jpeg, png, gif, svg',
+        ]    
+    );
+        //proses upload foto
+        if(!empty($request->foto)){
+            $fileName = 'foto-' . uniqid() . '.' . $request->foto->extension();
+            $request->foto->move(public_path('admin/img'), $fileName);
+        } else{
+            $fileName = '';
+        }
         //tambah data menggunakan query builder
         DB::table('produk')->insert([
             'kode'=>$request->kode,
@@ -46,6 +82,8 @@ class ProdukController extends Controller
             'harga_jual'=>$request->harga_jual,
             'stok'=>$request->stok,
             'min_stok'=>$request->min_stok,
+            'foto'=>$fileName,
+            'deskripsi'=>$request->deskripsi,
             'jenis_produk_id'=>$request->jenis_produk_id,
         ]);
         return redirect('admin/produk');
@@ -57,6 +95,12 @@ class ProdukController extends Controller
     public function show(string $id)
     {
         //
+        $produk = Produk::join('jenis_produk', 'jenis_produk_id', '=', 'jenis_produk.id')
+        ->select('produk.*', 'jenis_produk.nama as jenis')
+        ->where('produk.id', $id)
+        ->get();
+        return view ('admin.produk.detail', compact('produk'));
+
     }
 
     /**
@@ -65,6 +109,9 @@ class ProdukController extends Controller
     public function edit(string $id)
     {
         //
+        $jenis_produk = DB::table('jenis_produk')->get();
+        $produk = DB::table('produk')->where('id',$id)->get();
+        return view ('admin.produk.edit', compact('produk' , 'jenis_produk'));
     }
 
     /**
@@ -72,7 +119,55 @@ class ProdukController extends Controller
      */
     public function update(Request $request, string $id)
     {
-        //
+        $request->validate([
+            'nama' => 'required|max:45',
+            'harga_beli' => 'required|numeric',
+            'harga_jual' => 'required|numeric',
+            'stok' => 'required|numeric',
+            'min_stok' => 'required|numeric',
+            'foto' => 'nullable|image|mimes:jpg,jpeg,gif,png,svg|max:2048',
+            'deksripsi' => 'nullable|string|min:10',
+            'jenis_produk_id' => 'required|integer',
+        ],
+        [
+            'nama.required' => 'Nama wajib diisi',
+            'nama.max' => 'Nama maksimal 45 karakter',
+            'harga_beli.required' => 'Harga beli Harus diisi',
+            'harga_beli.numeric' => 'Harus Angka',
+            'harga_jual.required' => 'Harga jual harus disii',
+            'harga_jual.numeric' => 'harus angka',
+            'stok.required' => 'Stok harus diisi',
+            'min_stok.required' => 'Minimal stok harus terisi',
+            'foto.max' => 'Maksimal 2 MB',
+            'foto.image' => 'File ekstensi harus jpg,jpeg, png, gif, svg',
+        ] 
+    );
+        //update foto
+        $foto = DB::table('produk')->select('foto')->where('id', $request->id)->get();
+        foreach($foto as $f){
+            $namaFileFotoLama = $f->foto;
+        }
+        if(!empty($request->foto)){
+            //jika ada foto lama maka hapus fotonya 
+        if(!empty($namaFileFotoLama->foto)) unlink('admin/img'.$namaFileFotoLama->foto);
+        //proses ganti foto
+        $fileName = 'foto-'.$request->id . '.' . $request->foto->extension();
+        $request->foto->move(public_path('admin/img'), $fileName);
+        } else {
+            $fileName = '';
+        }
+        DB::table('produk')->where('id',$request->id)->update([
+            'kode'=>$request->kode,
+            'nama'=>$request->nama,
+            'harga_beli'=>$request->harga_beli,
+            'harga_jual'=>$request->harga_jual,
+            'stok'=>$request->stok,
+            'min_stok'=>$request->min_stok,
+            'foto'=>$fileName,
+            'deskripsi'=>$request->deskripsi,
+            'jenis_produk_id'=>$request->jenis_produk_id,
+        ]);
+        return redirect('admin/produk');
     }
 
     /**
@@ -81,5 +176,7 @@ class ProdukController extends Controller
     public function destroy(string $id)
     {
         //
+        DB::table('produk')->where('id', $id)->delete();
+        return redirect('admin/produk');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -23,30 +23,29 @@ use App\Http\Controllers\PelangganController;
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::get('salam', function () {
-    return ("ASSALAMUALAIKUM SELAMAT BELAJAR LARAVEL");
+Route::get('/salam', function(){
+    return "Assalamualaikum selamat belajar Laravel";
 });
 //tambah routing dengan parameter
-Route::get('/staff/{nama}/{divisi}', function ($nama, $divisi) {
+Route::get('/staff/{nama}/{divisi}', function($nama, $divisi){
     return 'Nama Pegawai : '.$nama. '<br> Departemen : '.$divisi;
 });
-
-//Routing dengan memanggil nama file dari view
-Route::get('/kondisi', function () {
+//routing dengan memanggil nama file dari view
+Route::get('/kondisi', function(){
     return view('kondisi');
 });
 Route::get('/nilai', function(){
     return view('coba.nilai');
 });
-
 //routing dengan view dan array data
-Route::get('/daftarnilai', function (){
-    return view ('coba.daftar');
+Route::get('/daftarnilai', function(){
+    return view('coba.daftar');
 });
 //routing manggil dari class controller
-Route::get('/dataMahasiswa', [LihatNilaiController::class, 'dataMahasiswa']);
+Route::get('/datamahasiswa', [LihatNilaiController::class, 'dataMahasiswa']);
 
+
+Route::prefix('admin')->group(function(){
 Route::get('/dashboard', [DashboardController::class, 'index']);
 //contoh pemanggilan secara satu persatu function menggunakan get,put, update, delete
 Route::get('/notfound', [PagenotController::class, 'index']);
@@ -55,7 +54,22 @@ Route::get('/notfound', [PagenotController::class, 'index']);
 Route::resource('kartu', KartuController::class);
 //memanggil fungsi satu persatu
 Route::get('/jenis_produk', [JenisProdukController::class, 'index']);
+Route::get('/jenis_produk/create', [JenisProdukController::class, 'create']);
+Route::post('/jenis_produk/store', [JenisProdukController::class, 'store']);
+Route::get('/jenis_produk/edit/{id}', [JenisProdukController::class, 'edit']);
+Route::post('/jenis_produk/update/{id}', [JenisProdukController::class, 'update']);
 
+//routing untuk table produk
 Route::get('/produk', [ProdukController::class, 'index']);
+Route::get('/produk/create', [ProdukController::class, 'create']);
+Route::post('/produk/store', [ProdukController::class, 'store']);
+Route::get('/produk/show/{id}', [ProdukController::class, 'show']);
+Route::get('/produk/edit/{id}', [ProdukController::class, 'edit']);
+Route::post('/produk/update/{id}', [ProdukController::class, 'update']);
+Route::get('/produk/delete/{id}', [ProdukController::class, 'destroy']);
+
+
 
 Route::resource('pelanggan', PelangganController::class);
+
+});
